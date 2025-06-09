b7d7776fcf794724cb229aaa5aeff9b604fd9f07
snake0818 <cs081807@gmail.com>
2025-06-09 21:46:21 +0800

    20250609 1.使用 Locomotive-scroll 實現滾動動畫，提升視覺體驗 2.移除 Theme toggle button 與 Navbar

---
+++ public/javascripts/scroll.js
@@ -0,0 +1,13 @@
+setTimeout((function () {
+    document.documentElement.classList.remove("is-loading");
+    document.documentElement.classList.add("is-loaded");
+    document.documentElement.classList.add("is-ready")
+}), 300);
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    tablet: {
+        breakpoint: 768
+    }
+});
