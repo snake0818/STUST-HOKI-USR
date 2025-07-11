fb44bad37c3f7f572d0a13a70ec8e14b77e0cbb8
snake0818 <cs081807@gmail.com>
2025-07-11 17:20:59 +0800

    20250711 1.改用 GSAP 製作視覺體驗 2.單獨 svg 檔案儲存所有 icon 3.重新構建整體網頁結構

--- public/javascripts/scroll.js
+++ public/javascripts/scroll.js
@@ -1,13 +1,29 @@
-setTimeout((function () {
-    document.documentElement.classList.remove("is-loading");
-    document.documentElement.classList.add("is-loaded");
-    document.documentElement.classList.add("is-ready")
-}), 300);
+gsap.registerPlugin(ScrollTrigger);
 
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true,
-    tablet: {
-        breakpoint: 1000
-    }
+ScrollTrigger.create({
+  trigger: ".c-summary",
+  start: "top center",
+  onEnter: () => {
+    document.querySelector(".c-summary").classList.add("inview");
+  },
+});
+
+ScrollTrigger.matchMedia({
+  "(min-width: 768px)": function () {
+    const growStory = gsap.timeline();
+    ScrollTrigger.create({
+      animation: growStory,
+      trigger: "#WhatDo",
+      start: "top top",
+      end: "bottom top",
+      scrub: 1,
+      pin: true,
+      anticipatePin: 3,
+    });
+    growStory
+      .from(".community-section", { xPercent: 0 })
+      .to(".community-section", { xPercent: -100 })
+      .to(".community-section", { xPercent: -200 })
+      .to(".community-section", { xPercent: -300 });
+  },
 });
